5e0e45d15d40a434919f7b8cbaab771fba7b5907
Roman Charugin <roman.charugin@gmail.com>
2013-12-26 04:40:21 -0500

    Get back comments cutting in grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,7 @@ module.exports = function( grunt ) {
       options: {
         mangle: {
           except: ['Swipe']
-        },
-        preserveComments: 'all'
+        }
       },
       dist: {
         files: {
